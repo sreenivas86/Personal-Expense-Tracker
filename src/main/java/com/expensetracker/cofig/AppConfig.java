f9975502743b245e0f49f51451ec6162e8ed6419
sreenivas86 <daramsreenivas8106@gmail.com>
2026-06-08 21:33:38 +0530

    properties file

--- src/main/java/com/expensetracker/cofig/AppConfig.java
+++ src/main/java/com/expensetracker/cofig/AppConfig.java
@@ -27,11 +27,11 @@ import com.expensetracker.service.impl.UserServiceImpl;
 @Configuration
 @EnableWebSecurity
 public class AppConfig {
-	
+
 	private UserServiceImpl userServiceImpl;
-	 @Value("${frontend.cors.list}")
-    private String origins;
-	
+	@Value("${frontend.cors.list}")
+	private String origins;
+
 	@Autowired
 	private JwtFilter jwtFilter;
 
@@ -39,65 +39,63 @@ public class AppConfig {
 		super();
 		this.userServiceImpl = userServiceImpl;
 	}
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
 	AuthenticationManager authManager(HttpSecurity http) throws Exception {
 		AuthenticationManagerBuilder auth = http.getSharedObject(AuthenticationManagerBuilder.class);
 		auth.userDetailsService(userServiceImpl).passwordEncoder(passwordEncoder());
 		return auth.build();
-				
+
 	}
-	
-//	@Bean 
-//	DaoAuthenticationProvider authProvider() {
-//		DaoAuthenticationProvider auth=new DaoAuthenticationProvider(userServiceImpl);
-//		auth.setPasswordEncoder(passwordEncoder());
-//		return auth;
-//	}
-	
-	
-	
-//	@Bean 
-//	AuthenticationManager authManager(AuthenticationConfiguration config) throws Exception {	
-//		return config.getAuthenticationManager();
-//	}
+
+	// @Bean
+	// DaoAuthenticationProvider authProvider() {
+	// DaoAuthenticationProvider auth=new
+	// DaoAuthenticationProvider(userServiceImpl);
+	// auth.setPasswordEncoder(passwordEncoder());
+	// return auth;
+	// }
+
+	// @Bean
+	// AuthenticationManager authManager(AuthenticationConfiguration config) throws
+	// Exception {
+	// return config.getAuthenticationManager();
+	// }
 
 	@Bean
 	SecurityFilterChain security(HttpSecurity http) throws Exception {
 
 		http.authorizeHttpRequests(req -> {
-			req.requestMatchers("/auth/**","/v3/api-docs/**",
-                    "/swagger-ui/**",
-                    "/swagger-ui.html").permitAll().anyRequest().authenticated();
+			req.requestMatchers("/auth/**", "/v3/api-docs/**",
+					"/swagger-ui/**",
+					"/swagger-ui.html").permitAll().anyRequest().authenticated();
 		});
 		http.csrf(csrf -> csrf.disable());
-		http.addFilterBefore(corsFilter(),UsernamePasswordAuthenticationFilter.class);
-		http.addFilterBefore(jwtFilter,UsernamePasswordAuthenticationFilter.class);
-		
+		http.addFilterBefore(corsFilter(), UsernamePasswordAuthenticationFilter.class);
+		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
 		// http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
-	
+
 	@Bean
-	CorsFilter  corsFilter() {
+	CorsFilter corsFilter() {
 		CorsConfiguration config = new CorsConfiguration();
 		List<String> allowedOrigins = Arrays.asList(origins.split(","));
-        config.setAllowedOrigins(allowedOrigins); // frontend origin
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true); // 
+		config.setAllowedOrigins(allowedOrigins); // frontend origin
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		config.setAllowedHeaders(List.of("*"));
+		config.setAllowCredentials(true); //
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
 
-        return new CorsFilter(source); 
+		return new CorsFilter(source);
 	}
-	
-	
-	
 
 }
